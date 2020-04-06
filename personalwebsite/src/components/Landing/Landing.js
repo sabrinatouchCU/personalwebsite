2264fa9ef705a07e7ce68f95e393d51cf6dac2c2
sabrinatouchCU <sabrina.touch@colorado.edu>
2020-04-06 15:55:35 -0600

    Minor changes

---
+++ personalwebsite/src/components/Landing/Landing.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Landing.css';
+
+function Landing() {
+  return (
+    <div className="Landing">
+      <header className="Landing-header">
+        I'm the Touch.
+        Sabrina Touch.
+        <a
+          className="Landing-link"
+          href="https://www.youtube.com/user/sabrinatouchhh"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Landing-Link
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default Landing;
